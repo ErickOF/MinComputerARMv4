fa5a049678fbe3cfec8cf0b78ddafdcd337f0ff2
reiracm <reiracm@gmail.com>
2019-11-19 23:58:50 -0600

    Update ARMStruct.sv

--- Processorv2/ARMStruct.sv
+++ Processorv2/ARMStruct.sv
@@ -3,13 +3,12 @@ module ARMStruct(input logic clk, reset,
 					  output logic [31:0] PC, ALUResult, WriteData,
 					  output logic MemWrite);
 	
-	logic Z, N, V, C;
 	logic [3:0] ALUFlags;
 	logic RegWrite, ALUSrc, MemtoReg, PCSrc;
 	logic [1:0] RegSrc, ImmSrc;
 	logic [1:0] ALUControl;
 	
 	ControlUnit controller(clk, reset, Instr[31:5],ALUFlags, RegSrc, RegWrite, ImmSrc, ALUSrc, ALUControl, MemWrite, MemtoReg, PCSrc);
-	datapath Datapath(clk, reset, RegSrc, RegWrite,ImmSrc,ALUSrc, ALUControl, MemtoReg, PCSrc, Z, N, V, C, PC, Instr, ALUResult, WriteData, ReadData);
+	datapath Datapath(clk, reset, RegSrc, RegWrite,ImmSrc,ALUSrc, ALUControl, MemtoReg, PCSrc, ALUFlags, PC, Instr, ALUResult, WriteData, ReadData);
 
 endmodule
