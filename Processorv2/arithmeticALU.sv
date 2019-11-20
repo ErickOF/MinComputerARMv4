048eb05e78b820f34bcf845228b8e23161659fae
reiracm <reiracm@gmail.com>
2019-11-20 02:20:07 -0600

    Update arithmeticALU.sv

--- Processorv2/arithmeticALU.sv
+++ Processorv2/arithmeticALU.sv
@@ -8,10 +8,12 @@ module arithmeticALU	#(parameter n = 32)(input logic [n-1:0] a, b,
 	logic [n-1:0] result_sum, result_sub;
 	logic [n-1:0] result_mux_not;
 	
-	fulladder fulladder_module (a, result_mux_not, 1'b0, result_sum, Cout);
+	fulladder fulladder_module (a,b, 1'b0, result_sum, Cout);
 	
-	mux2x1_nBits #(n) m1(Y, result_sum, result_sub, ALUControl);
+	Substractor_nBits #(n) sub(a, b, 1'b0, Bout, result_sub);
 	
-	mux2x1_nBits #(1) m2(Y_CB, Cout, Bout, ALUControl);
+	mux2x1_nBits #(n) m1(result_sum, result_sub, ALUControl, Y);
+	
+	mux2x1_nBits #(1) m2(Cout, Bout, ALUControl, Y_CB);
 	
 endmodule
